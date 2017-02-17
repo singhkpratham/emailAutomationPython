0d56aaefd49c0e88b14c9db5438dc17727d9414c
Kumar Singh <Kumar.Singh@mu-sigma.com>
2017-02-17 10:54:07 +0530

    collates the sharepoint excel sheet and creates a csv

--- collate.py
+++ collate.py
@@ -18,7 +18,6 @@ def spfetcher(spLink, saveTo):
     return(df)
 df = spfetcher(spLink, saveTo)
 
-a = df.count()
 
 for i in range(0,df.shape[1]):    
     for j in range(0,df.shape[0]):        
@@ -27,17 +26,38 @@ for i in range(0,df.shape[1]):
             print(j , i)
         except:
             try:
-                if bool(re.search("NA|NO" , df.ix[j,i], re.I)):
+                if bool(re.search("NA|NO|Yes" , df.ix[j,i], re.I)):
                     df.ix[j,i] = 0
             except:
                 pass
 
+noRed   =          df.ix[:,10].sum()
+noGreen =          df.ix[:,11].sum()
+noTotalRedGreen =  noRed + noGreen
+perRed  =          round(noRed/noTotalRedGreen*100)
+perGreen =         round(noGreen/noTotalRedGreen*100)
+noTotalDel =       df.ix[:,8].sum()
+noGroups =         df.ix[:,0].count()
+noErrors =         df.ix[:,3].sum()
+noRCA    =         df.ix[:,5].sum()
+noScoreb =         df.ix[:,1].count()
+noLessThan2Weeks = df.ix[df.ix[:,2] < 2,2].count()
+noMoreThan6Weeks = df.ix[df.ix[:,2] > 6,2].count()
 
-##collate = pd.DataFrame({
-##    'FU' : [5],
-##    'Response Time':[''],
-##    '# of scoreboards | # Subgroups':['%s|%s'%(a[1],a[0])],
-##    '# of errors this week|# of RCAs received by FUL':['%s|%s'%(df.ix[:,3].sum(),df.ix[:,5].sum()],
-##})
-##
+collate = pd.DataFrame({
+    'FU' : [5],
+    'Response Time':[''],
+    '# of scoreboards | # Subgroups':['%s|%s'%(noScoreb,noGroups)],
+    '# of errors this week|# of RCAs reviewed by FUL':['%s|%s'%(noErrors,noRCA)],
+    '# of groups < 2 weeks since last error (%)' : ['%s (%d%%)' %(noLessThan2Weeks, round(noLessThan2Weeks / df.ix[:,2].count()*100))],
+    '# of groups > 6 weeks since last error (%)' : ['%s (%d%%)' %(noMoreThan6Weeks, round(noMoreThan6Weeks / df.ix[:,2].count()*100))],
+    '# red (%)+ | # green (%)+ | # Tracked (%)++ | Total # ' : ['%s(%s%%)|%s(%s%%)|%s(%s%%)|%s' %( noRed,perRed ,noGreen  ,perGreen ,
+                                                                                             noTotalRedGreen   ,round(noTotalRedGreen/noTotalDel*100)    ,noTotalDel ) ]                                                       
+})
+
+collate = collate[['FU','Response Time' ,'# of scoreboards | # Subgroups' , '# of errors this week|# of RCAs reviewed by FUL',
+                   '# of groups < 2 weeks since last error (%)','# of groups > 6 weeks since last error (%)' ,'# red (%)+ | # green (%)+ | # Tracked (%)++ | Total # ' ]]
+
+collate.to_csv("collated.csv", index = False)
+
 
